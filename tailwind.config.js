95ba02ba66a7cdabb8bac01c0d3913695afcf63a
Andrii Chychkan <mywork1728@gmail>
2023-05-23 21:41:25 +0300

    added main comtent

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainColor: '#FCF7E6'
+      }
+    },
   },
   plugins: [],
 }
